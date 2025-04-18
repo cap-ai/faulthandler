ceb179345b8f9837dc112fd48ae1ca8c4ef04ae7
cap-ai <zaharmax@gmail.com>
2025-04-18 13:23:43 +0200

    Add initial documentation structure and styles for faulthandler project

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+# Minimal Sphinx configuration
+project = 'faulthandler'
+copyright = '2025'
+author = 'Author'
+
+# Required Sphinx stubs
+extensions = []
+master_doc = 'index'
+language = 'en'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Basic theme setting
+html_theme = 'alabaster'
